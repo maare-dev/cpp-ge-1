eaca7ef076ab9f8e3790dfcf4eb99209564847e8
maare-dev <aleksandr.homewrk@gmail.com>
2026-07-07 12:48:08 +0300

    Added: modules: Component Entity Scene, lite body of int main()

---
+++ engine/entity/Entity.cppm
@@ -0,0 +1,5 @@
+export module engine.entity.Entity;
+
+export class Entity{
+
+};
--- main.cpp
+++ main.cpp
@@ -1,8 +1,24 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/Window/VideoMode.hpp>
 #include <SFML/Window/Window.hpp>
+#include "engine/define/info.hpp"
 
 int main(){
-    sf::Window window(sf::VideoMode({800, 600}), "");
+    sf::RenderWindow window(sf::VideoMode({800, 600}), ENGINE_NAME);
+
+    while(window.isOpen()){
+        sf::Event event;
+        while(window.pollEvent(event)){
+            //TODO: give each event to scene
+            //TODO: delete this event checking
+            if(event.type == sf::Event::Closed){
+                window.close();
+            }
+        }
+        //TODO: update scene
+        window.clear();
+        //TODO: draw scene
+        window.display();
+    }
     return 0;
 }
